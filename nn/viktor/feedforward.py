fix gradient

nn/viktor/feedforward.py
@@ -73,11 +73,11 @@ class Net(nn.Module):
     def batch(self, x, y, loss_func):
         yhat = self.forward(x)
         loss = loss_func(yhat, y)
+        self.optimizer.zero_grad()
         loss.backward()
         for name, param in self.named_parameters():
             print(name, param.grad.abs().sum())
         self.optimizer.step()
-        self.optimizer.zero_grad()
         return loss.item()
          
     def graph_loss(self):

nn/viktor/main.py
@@ -36,7 +36,7 @@ def feedforward():
     # based on https://www.analyticsvidhya.com/blog/2019/10/building-image-classification-models-cnn-pytorch/
     model = Net(W)
     # defining the optimizer
-    model.optimizer = Adam(model.parameters(), lr=0.07)
+    model.optimizer = Adam(model.parameters(), lr=0.005)
     # defining the loss function
     criterion = nn.MSELoss()
     # checking if GPU is available