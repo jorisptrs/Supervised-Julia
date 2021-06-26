3aa96739d73da6c0560d5e601a95b734ea50cd84
Joris Peters <joris@ptrs.ai>
2021-06-26 13:31:48 +0200

    started commenting changed the data-set normalization

--- nn/viktor/data.py
+++ nn/viktor/data.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import torch.utils.data as tdata
 
@@ -7,10 +6,12 @@ import zipfile
 import os
 import time
 import multiprocessing
-
+from sklearn.preprocessing import StandardScaler
 
 class JuliaDataset(tdata.Dataset):
-
+    """
+    Our custom dataset.
+    """
     def __init__(self, num_cores, debug=True):
         self.num_cores = num_cores
         self.debug = debug
@@ -24,30 +25,53 @@ class JuliaDataset(tdata.Dataset):
         self.minY = 0
 
     def __len__(self):
+        """
+        Returns the training set size
+        """
         return self.num_images
     
     def __getitem__(self, idx):
+        """
+        Returns features and labels at index idx
+        """
         return (self.x[idx], self.y[idx])
 
     def read_header(self, path, header_name="header.txt"):
+        """
+        Read the meta-data from the header file.
+        """
         with open(os.path.join(path, header_name)) as f:
             for line in f.readlines():
                 line = line.strip().split("=")
                 self.meta_data[line[0]] = line[1]
-                        
+            
             self.maxX = int(self.meta_data["ITERATIONS"])
             self.maxY = float(self.meta_data["SAMPLE_RADIUS"])
             self.minY = -self.maxY
 
     def reader(self, filename):
+        """
+        Return an array from a comma-separated file
+        """
         return np.genfromtxt(filename, delimiter=",")
 
-    def normalize(self, lower, upper, x):
-        magnitude = upper - lower
-        nX = (x - lower) / magnitude
-        return nX
+    def normalize(self, x):
+        """
+        Normalize the features such that each pixel-value is between 0 and 1.
+        Standardize to mean = 0 and std = 1.
+        """
+        old_shape = x.shape
+        x = x.reshape(old_shape[0], -1)
+        scaler = StandardScaler().fit(x)
+        x = scaler.transform(x)
+        x = x.reshape(old_shape)
+        return x
 
     def load_images(self, path, num_images, pooling=True, download=True):
+        """
+        After ensuring that the dataset is locally present, load features X and labels Y into their
+        corresponding fields, both as numpy arrays.
+        """
         self.num_images = num_images
         file_names = []
 
@@ -73,7 +97,7 @@ class JuliaDataset(tdata.Dataset):
                 self.x.append(tempX)
 
         self.x = np.array(self.x, dtype=np.float)
-        self.x = self.normalize(self.minX, self.maxX, self.x)
+        self.x = self.normalize(self.x)
         self.y = self.reader(os.path.join(path, "labels.txt"))
         self.y = self.y[:num_images]
 
@@ -84,6 +108,7 @@ class JuliaDataset(tdata.Dataset):
 
     def download_data(self, path, google_drive_id='13jpZFAuGekt3qZoikFs5VaD-0XUO8zdc'):
         """
+        Download the dataset from google drive to data the 'data'-folder.
         based on https://github.com/ndrplz/google-drive-downloader
         """      
         url = "https://docs.google.com/uc?export=download"
