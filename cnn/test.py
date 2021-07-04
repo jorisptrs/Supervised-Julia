6527d27091caf90c10513c7f9af1e8b3e8dafae7
Viktor Vesely <30593536+viktorvesely@users.noreply.github.com>
2021-07-04 17:13:53 +0200

    Structure

---
+++ cnn/test.py
@@ -0,0 +1,55 @@
+import os
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+
+from feedforward import CNN
+import data
+import main
+import save
+
+ALL_DATA = -1
+TEST_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','testData')
+
+
+def final_exam(model):
+    """
+    Performs an intensive test on the trained model
+    """
+    test_ds = data.JuliaDataset(TEST_PATH, ALL_DATA, main.CORES, False)
+    test_df = torch.utils.data.DataLoader(test_ds, shuffle=True, batch_size=len(test_ds), num_workers=main.CORES)
+    _, y, yhat =  model.compute_error(test_df, nn.L1Loss().to(main.DEVICE), main.DEVICE)
+
+    y = y[0].numpy()
+    yhat = yhat[0].numpy()
+
+    loss = np.power(y - yhat, 2)
+    loss = np.sum(loss, axis=1)
+
+    y = np.transpose(y)
+    yhat = np.transpose(yhat)
+
+    df = pd.DataFrame({
+        "yreal" : y[0],
+        "yimag" : y[1],
+        "loss" : loss,
+        "yhatreal": yhat[0],
+        "yhatimag": yhat[1]
+    })
+    df.to_csv(os.path.join(TEST_PATH, "loss.csv"), index=False) 
+
+def count_parameters(model):
+    """
+    Return the amount of parameters
+    """
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+if __name__ == "__main__":
+    
+    config = {'lr' : 0.03, 'alpha' : 0} # doesn't really matter for testing
+    model = CNN(config)
+    save.model_load(model, os.path.join(main.MODEL_PATH, main.MODEL_NAME))
+
+    # print(count_parameters(model))
+    final_exam(model)
---
+++ data-graphing/colorplot.py
@@ -0,0 +1,26 @@
+# code from https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.griddata.html
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from scipy.interpolate import griddata
+
+df = pd.read_csv("../regression/testData/loss.csv")
+
+points = df[["yreal", "yimag"]].to_numpy()
+z = df["loss"].to_numpy()
+
+r = 3.2
+
+grid_x, grid_y = np.mgrid[-r:r:1000j, -r:r:1000j]
+Z = griddata(points, z, (grid_x, grid_y), method='cubic', fill_value=100)
+
+
+plt.imshow(Z, extent=(-r,r,-r,r), vmin=0, vmax=7, origin='lower', cmap="jet")
+plt.title("Interpolated prediction error")
+plt.xlabel('Real', fontsize=10)
+plt.ylabel('Imaginary', fontsize=10)
+clb=plt.colorbar()
+clb.ax.tick_params(labelsize=8) 
+clb.ax.set_title('Loss',fontsize=10)
+plt.show()
